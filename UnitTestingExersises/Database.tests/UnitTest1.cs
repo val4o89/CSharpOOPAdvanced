4045414ea274cc6ff4fd0d383843a2587e83aa29
val4o89 <valentinvalentinovv@abv.bg>
2016-08-27 15:13:29 +0300

    Add files via upload

---
+++ UnitTestingExersises/Database.tests/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P01Database;
+
+namespace Databasee.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Database database;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            database = new Database();
+        }
+
+        [TestMethod]
+        public void TestDatabaseSize()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                database.Add(i);
+            }
+            Assert.AreEqual(16, database.Count, $"Database size is {database.Count}");
+        }
+
+        [TestMethod]
+        public void TestAddTwoElements()
+        {
+            database.Add(21);
+            database.Add(45);
+            Assert.AreEqual(2, database.Count, $"Database size is {database.Count}");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestByAddingOverThanSixteenElements()
+        {
+            for (int i = 0; i < 18; i++)
+            {
+                database.Add(i);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestRemoveElementWhenTheCountOfDatabaseIsZero()
+        {
+            database.Remove();
+        }
+
+        [TestMethod]
+        public void TestConstructorToAddSixteenElements()
+        {
+            database = new Database(new int[16]);
+            Assert.AreEqual(16, database.Count);
+        }
+
+        [TestMethod]
+        public void TestFetchMethodIfReturnTheCorrectCollection()
+        {
+            int[] expectedCollection = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            for (int i = 1; i <= 16; i++)
+            {
+                database.Add(i);
+            }
+            int[] fetchedCollection = database.Fetch();
+
+            CollectionAssert.AreEqual(expectedCollection, fetchedCollection);
+            
+        }
+    }
+
+}
---
+++ UnitTestingExersises/P01Database/Database.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01Database
+{
+    public class Database
+    {
+        private int[] array;
+        private int count;
+
+        public Database()
+        {
+            this.array = new int[16];
+        }
+
+
+        public Database(int[] ints)
+        {
+            this.array = new int[16];
+            for (int i = 0; i < ints.Length; i++)
+            {
+                this.array[i] = ints[i];
+                count++;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public int[] Array
+        {
+            get
+            {
+                return this.array;
+            }
+        }
+
+        public void Add(int number)
+        {
+            if (count == 16)
+            {
+                throw new InvalidOperationException();
+            }
+            array[count] = number;
+            count++;
+        }
+
+        public void Remove()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            array[count - 1] = 0;
+            count--;
+        }
+
+        public int[] Fetch()
+        {
+            int[] arrayToReturn = new int[this.count];
+
+            for (int i = 0; i < arrayToReturn.Length; i++)
+            {
+                arrayToReturn[i] = array[i];
+            } 
+            return arrayToReturn;
+        }
+
+    }
+}
